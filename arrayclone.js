ca93ce5b88465eacc58502a9372ce7876a955b63
Aniruddh0402 <anirudha007.ac@gmail.com>
2022-07-12 12:40:00 +0530

    array

---
+++ arrayclone.js
@@ -0,0 +1,9 @@
+let array1 =["ani","manish","prashan"];
+
+// let array2 = array1.slice(0);
+// let array2 = [].concat(array1);
+//spread operator
+let array2 = [...array1,"akashay","rajat"];
+
+console.log(array1);
+console.log(array2);
